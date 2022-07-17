f0eb5b24f06814c50bb283f6467e29f13309a1b9
LV-IBerget <iberget@launchvox.com>
2022-07-16 22:35:47 -0700

    IB, converted for use with JS lib

--- Source/OscLiveLink/Private/OscLiveLinkComponent.cpp
+++ Source/OscLiveLink/Private/OscLiveLinkComponent.cpp
@@ -43,6 +43,7 @@ void UOscLiveLinkComponent::InitializeSubject()
 	Subjects.Add(FName(TEXT("OscHead")));
 
 	ShapeNames.Reserve(55);
+	// Mac OSC List ///////
 	ShapeNames.Add(FName(TEXT("BrowInnerUp")));
 	ShapeNames.Add(FName(TEXT("BrowDownLeft")));
 	ShapeNames.Add(FName(TEXT("BrowDownRight")));
@@ -98,7 +99,69 @@ void UOscLiveLinkComponent::InitializeSubject()
 	
 	ShapeNames.Add(FName(TEXT("headYaw")));
 	ShapeNames.Add(FName(TEXT("headPitch")));
+	ShapeNames.Add(FName(TEXT("headRoll")));*/
+	/////////////////
+
+	//From JS Lib
+	// ShapeNames.Reserve(55);
+	ShapeNames.Add(FName(TEXT("BrowDownLeft")));
+	ShapeNames.Add(FName(TEXT("BrowDownRight")));
+	ShapeNames.Add(FName(TEXT("BrowInnerUp")));
+	ShapeNames.Add(FName(TEXT("BrowOuterUpLeft")));
+	ShapeNames.Add(FName(TEXT("BrowOuterUpRight")));
+	ShapeNames.Add(FName(TEXT("CheekSquintLeft")));
+	ShapeNames.Add(FName(TEXT("CheekSquintRight")));
+	ShapeNames.Add(FName(TEXT("EyeBlinkLeft")));
+	ShapeNames.Add(FName(TEXT("EyeBlinkRight")));
+	ShapeNames.Add(FName(TEXT("EyeLookDownLeft")));
+	ShapeNames.Add(FName(TEXT("EyeLookDownRight")));
+	ShapeNames.Add(FName(TEXT("EyeLookInLeft")));
+	ShapeNames.Add(FName(TEXT("EyeLookInRight")));
+	ShapeNames.Add(FName(TEXT("EyeLookOutLeft")));
+	ShapeNames.Add(FName(TEXT("EyeLookOutRight")));
+	ShapeNames.Add(FName(TEXT("EyeLookUpLeft")));
+	ShapeNames.Add(FName(TEXT("EyeLookUpRight")));
+	ShapeNames.Add(FName(TEXT("EyeSquintLeft")));
+	ShapeNames.Add(FName(TEXT("EyeSquintRight")));
+	ShapeNames.Add(FName(TEXT("EyeWideLeft")));
+	ShapeNames.Add(FName(TEXT("EyeWideRight")));
+	ShapeNames.Add(FName(TEXT("JawLeft")));
+	ShapeNames.Add(FName(TEXT("JawOpen")));
+	ShapeNames.Add(FName(TEXT("JawRight")));
+	ShapeNames.Add(FName(TEXT("MouthClose")));
+	ShapeNames.Add(FName(TEXT("MouthDimpleLeft")));
+	ShapeNames.Add(FName(TEXT("MouthDimpleRight")));
+	ShapeNames.Add(FName(TEXT("MouthFrownLeft")));
+	ShapeNames.Add(FName(TEXT("MouthFrownRight")));
+	ShapeNames.Add(FName(TEXT("MouthFunnel")));
+	ShapeNames.Add(FName(TEXT("MouthLeft")));
+	ShapeNames.Add(FName(TEXT("MouthLowerDownLeft")));
+	ShapeNames.Add(FName(TEXT("MouthLowerDownRight")));
+	ShapeNames.Add(FName(TEXT("MouthPressLeft")));
+	ShapeNames.Add(FName(TEXT("MouthPressRight")));
+	ShapeNames.Add(FName(TEXT("MouthPucker")));
+	ShapeNames.Add(FName(TEXT("MouthRight")));
+	ShapeNames.Add(FName(TEXT("MouthRollLower")));
+	ShapeNames.Add(FName(TEXT("MouthRollUpper")));
+	ShapeNames.Add(FName(TEXT("MouthShrugLower")));
+	ShapeNames.Add(FName(TEXT("MouthShrugUpper")));
+	ShapeNames.Add(FName(TEXT("MouthSmileLeft")));
+	ShapeNames.Add(FName(TEXT("MouthSmileRight")));
+	ShapeNames.Add(FName(TEXT("MouthStretchLeft")));
+	ShapeNames.Add(FName(TEXT("MouthStretchRight")));
+	ShapeNames.Add(FName(TEXT("MouthUpperUpLeft")));
+	ShapeNames.Add(FName(TEXT("MouthUpperUpRight")));
+	ShapeNames.Add(FName(TEXT("NoseSneerLeft")));
+	ShapeNames.Add(FName(TEXT("NoseSneerRight")));
+	ShapeNames.Add(FName(TEXT("CheekPuff")));
+	ShapeNames.Add(FName(TEXT("JawForward")));
+	ShapeNames.Add(FName(TEXT("TongueOut")));
+
+	ShapeNames.Add(FName(TEXT("headYaw")));
+	ShapeNames.Add(FName(TEXT("headPitch")));
 	ShapeNames.Add(FName(TEXT("headRoll")));
+	////
+
 
 	FLiveLinkStaticDataStruct StaticDataStruct(FLiveLinkBaseStaticData::StaticStruct());
 	FLiveLinkBaseStaticData* BaseStaticData = StaticDataStruct.Cast<FLiveLinkBaseStaticData>();
